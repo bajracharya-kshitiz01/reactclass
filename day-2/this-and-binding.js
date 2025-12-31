ef6f24476c823a0bfe770b0be86f38dca1319ced
bajracharya-kshitiz01 <bajracharyak01@gmail.com>
2025-12-31 10:12:18 +0545

    console1

--- day-2/this-and-binding.js
+++ day-2/this-and-binding.js
@@ -1,1 +1,11 @@
-console.log(hello)
+let myobj={
+    fullname:"Ram Nepali",
+    age:40,
+    address:"KTM",
+    gender:"Male",
+    displayfullname: function(){
+        console.log("display full name function called")
+    },
+}
+//calling displayfullname functiions
+myobj.displayfullname()
